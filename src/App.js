33934c69824a4e7eccfe7e35c29d727daa21205e
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-07-09 16:05:42 +0500

    fixed user store

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import { useEffect } from "react";
+import { setCurrentUser } from "./store/user/user.action";
+import { useDispatch } from "react-redux";
+import { onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
