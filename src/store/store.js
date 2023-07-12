af52c6a18fa77bbf11dcff84d78eb0fa4295d6df
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-07-12 13:02:33 +0500

    added categories and cart tookits

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,8 @@
-// import { compose, createStore, applyMiddleware } from "redux";
+// import { compose, createStore, applyMiddleware } from 'redux';
 import { configureStore } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import logger from "redux-logger";
-// import createSagaMiddleware from "redux-saga";
-
-// import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
 
@@ -13,31 +10,26 @@ const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   whitelist: ["cart"],
-// };
-
-// const sagaMiddleware = createSagaMiddleware();
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 // const composeEnhancer =
-//   (process.env.NODE_ENV !== "production" &&
+//   (process.env.NODE_ENV !== 'production' &&
 //     window &&
 //     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
 //   compose;
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: middleWares,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleWares),
 });
-
-// createStore(persistedReducer, undefined, composedEnhancers);
-
-// sagaMiddleware.run(rootSaga);
 
 // export const persistor = persistStore(store);
