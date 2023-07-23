e1e9929a4e1e48bb6181faf0fc7ca6d1b98e184b
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-07-23 16:38:01 +0500

    sagas converted to ts from js

--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -37,7 +37,7 @@ export type SignInFailed = ActionWithPayload<
 
 export type SignUpStart = ActionWithPayload<
   USER_ACTION_TYPES.SIGN_UP_START,
-  { email: string }
+  { email: string; password: string; displayName: string }
 >;
 
 export type SignUpSuccess = ActionWithPayload<
@@ -78,7 +78,7 @@ export const emailSignInStart = withMatcher(
 );
 
 export const signInSuccess = withMatcher(
-  (user: UserData): SignInSucces =>
+  (user: UserData & { id: string }): SignInSucces =>
     createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
